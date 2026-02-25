3386d59153586c79e9984d2ac26bbcbbe1d7a5a5
Dishant Singh <dishantsinghdev@icloud.com>
2026-02-25 10:47:05 +0530

    added all kinds of favicons + improved root metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,41 @@ import Providers from "@/components/Providers";
 import NextTopLoader from 'nextjs-toploader';
 import { PaddleInit } from '@/components/paddle-init';
 
+const BASE_URL = 'https://www.freecustom.email';
+
 export const metadata = {
+  metadataBase: new URL(BASE_URL),
+
   title: 'Custom Temp Mail – Fastest Ad‑Free Disposable Email',
   description: 'Generate a custom temp mail address instantly—choose from multiple domains, no registration, no ads, forever free.',
   keywords: 'temp mail, disposable email, custom mail, fake email, temporary inbox, no ads email, 10minmail, email privacy, free temp mail',
+  alternates: {
+    canonical: '/en',
+  },
+
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      {
+        url: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        url: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+
+    apple: '/apple-touch-icon.png',
+    shortcut: '/favicon.ico',
+  },
+
+  manifest: '/site.webmanifest',
+
   openGraph: {
     title: 'Custom Temp Mail – Fastest Ad‑Free Disposable Email',
     description: 'Generate a custom temp mail address instantly—choose from multiple domains, no registration, no ads, forever free.',
@@ -28,10 +59,10 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       <head>
         <GoogleAnalytics gaId="G-RXTEEVC8C4" />
         <link rel="icon" href="/favicon.ico" />
-        <meta name='impact-site-verification' value='7df37ce6-8617-4606-8ba2-9a78bf367429'/>
+        <meta name='impact-site-verification' content='7df37ce6-8617-4606-8ba2-9a78bf367429' />
       </head>
       <body>
-        <NextTopLoader 
+        <NextTopLoader
           color="#2299DD"
           initialPosition={0.08}
           crawlSpeed={200}
